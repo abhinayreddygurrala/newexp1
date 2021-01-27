6f4541bfe35fc4f81a32c036899d9014d3cf3368
abhinayreddygurrala <gurralaabhinay@gmail.com>
2021-01-27 12:25:00 +0530

    error

---
+++ exp1.py
@@ -0,0 +1,27 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers# Ts program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+s program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+his program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
